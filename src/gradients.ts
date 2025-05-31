ffc9cfebfb046617da842cc13c1b4b0ec6e6184a
felipecarrillo100 <felipecarrillo100@gmail.com>
2025-05-31 19:42:26 +0200

    Remove default gradients but keep one in GrayScale

--- src/gradients.ts
+++ src/gradients.ts
@@ -1,38 +1,5 @@
 import {ColorMap, createGradientColorMap} from "@luciad/ria/util/ColorMap";
 
-export const WorldElevationColorMap= createGradientColorMap([
-    { level: 0.0, color: "#006a6a" },// Deep ocean (-400 m)
-    { level: 0.2, color: "#009999" },// Deep ocean (-400 m)
-    { level: 0.37, color: "#94D4D4" }, // Shallow water (400 m)
-    { level: 0.402, color: "#567C41" }, // Coastal lowlands (1200 m)
-    { level: 0.45, color: "#77A85A" }, // Lowlands (2700 m)
-    { level: 0.65, color: "#f2d671" }, // Highlands (4000 m)
-    { level: 0.78, color: "#ff8000" }, // Mountains (5300 m)
-    { level: 0.88, color: "#a6421c" }, // Very high mountains (6600 m)
-    { level: 0.96, color: "#9f9f9f" }, // Very high mountains (6600 m)
-    { level: 1.0, color: "#ffffff" }   // Extreme elevations (8000 m)
-
-]);
-
-export const Rainbow = createGradientColorMap([
-    { "level": 0, "color": "#ff0000" },
-    { "level": 0.0667, "color": "#ff9c00" },
-    { "level": 0.1333, "color": "#fff700" },
-    { "level": 0.2, "color": "#9cff00" },
-    { "level": 0.2667, "color": "#00ff24" },
-    { "level": 0.3333, "color": "#00ffb4" },
-    { "level": 0.4, "color": "#00f0ff" },
-    { "level": 0.4667, "color": "#009eff" },
-    { "level": 0.5333, "color": "#0048ff" },
-    { "level": 0.6, "color": "#4400ff" },
-    { "level": 0.6667, "color": "#ae00ff" },
-    { "level": 0.7333, "color": "#ff00d7" },
-    { "level": 0.8, "color": "#ff008c" },
-    { "level": 0.8667, "color": "#ff0049" },
-    { "level": 0.9333, "color": "#ff001d" },
-    { "level": 1, "color": "#ff0000" }
-]);
-
 export const GrayscaleGradient = createGradientColorMap([
     { "level": 0, "color": "#000000" },  // Black
     { "level": 0.0667, "color": "#111111" },
